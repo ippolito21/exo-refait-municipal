af689bd997bf7249d1e3cdcd065610c6e03f8d46
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-03-10 12:04:14 +0100

    mon premier commit

---
+++ muni.js
@@ -0,0 +1,4 @@
+quand on clique sur ce bouton  la liste des personnes candidates doit s'aaficher en dessousdu bouton.
+utiliser innrHtml
+amélorr la page : afficher le nom des personnes candidates, m
+document.quer
