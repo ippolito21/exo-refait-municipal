121c8d58c8dde7c9755049c91477127f5b85ed37
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-03-10 21:20:50 +0100

    ajout des balises pour finir le devoir

--- muni.js
+++ muni.js
@@ -1,4 +1,25 @@
-quand on clique sur ce bouton  la liste des personnes candidates doit s'aaficher en dessousdu bouton.
-utiliser innrHtml
-amélorr la page : afficher le nom des personnes candidates, m
-document.querySelector('.afficher-liste des participants')
+//mettre un bouton "afficher la liste des participants" sur la page.
+//quand on clique sur ce bouton  la liste des personnes candidates doit s'aaficher en dessousdu bouton.
+//utiliser innrHtml.
+//améliorer la page : afficher le nom des personnes participants, mais cette fois-ci sous forme de liste Html. (utiliser innerHTML) afficher la liste de participants en ligne droite
+let participants = [
+  "julien funais",
+  "corienne ganne",
+  "mathalie baume",
+  "Laurent hum",
+];
+
+document.querySelector('.afficher-liste').addEventListener('click', function() {
+  document.querySelector('.liste-participants').innerHTML = "";
+  participants.forEach(function(participant) {
+    document.querySelector('.liste-participants').innerHTML += '<li>' + participant + '</li>'
+  })
+});
+document.querySelector('.ajout-participant').addEventListener('click', function(){
+  paticipants.push(document.querySelector('.insertion-participant').value);
+
+document.querySelector('.list-participants').innerHTML ="";
+participants.forEach(function(participant){
+  document.querySelector('.liste-participants').innerHTML += '<li>' + participant + '</li>';
+  })
+});
