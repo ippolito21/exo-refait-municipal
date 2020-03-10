2645e2601094c5d3de277f01973cc13a9ea19b32
ippolito21 <60745255+ippolito21@users.noreply.github.com>
2020-03-10 16:05:37 +0100

    ajoute de button

--- muni.js
+++ muni.js
@@ -1,4 +1,4 @@
 quand on clique sur ce bouton  la liste des personnes candidates doit s'aaficher en dessousdu bouton.
 utiliser innrHtml
 amélorr la page : afficher le nom des personnes candidates, m
-document.quer
+document.querySelector('.afficher-liste des participants')
